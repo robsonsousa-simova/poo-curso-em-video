bf8a81150e74fd920bb0b2118208181c9bfc60ba
Robson Sousa <robson.sousa@simova.com.br>
2022-11-01 13:47:53 -0300

    'Aula8b - Vídeo 17': criação dos métodos 'marcarLuta' e 'apresentarLutadores', 'lutar' da classe 'Lutar' e outros ajustes no método 'isLutaAprovada' da classe 'Lutar'. Pequenos ajustes na classe 'UECCombate'.

--- src/main/java/br/com/simova/aula8b_video17/Luta.java
+++ src/main/java/br/com/simova/aula8b_video17/Luta.java
@@ -1,10 +1,18 @@
 package br.com.simova.aula8b_video17;
 
+import java.util.Random;
+
 public class Luta {
 
     private Lutador desafiado;
     private Lutador desafiante;
     private int rounds;
+
+    private boolean lutaAprovada;
+
+    public Luta() {
+
+    }
 
     public Luta(Lutador desafiado, Lutador desafiante, int rounds) {
         this.desafiado = desafiado;
@@ -36,12 +44,15 @@ public class Luta {
         this.rounds = rounds;
     }
 
-    public boolean isLutaAprovada() {
+    public boolean isLutaAprovada(Lutador desafiado, Lutador desafiante) {
 
-        if (desafiado.getCategoria().equals(desafiante.getCategoria())) {
+        if (desafiado.getCategoria().equals(desafiante.getCategoria()) && !desafiado.equals(desafiante)) {
             System.out.println("Luta aprovada. Pois tanto desafiado(" + desafiado.getNome()
                     + "), quanto desafiante(" + desafiante.getNome() + ") estão na categoria "
                     + desafiado.getCategoria() + ".");
+            this.lutaAprovada = true;
+            this.setDesafiado(desafiado);
+            this.setDesafiante(desafiante);
             return true;
         } else {
             System.out.println("Luta não aprovada. O desafiado ("
@@ -49,8 +60,69 @@ public class Luta {
                     + desafiado.getCategoria() + " e o desafiante ("
                     + desafiante.getNome() + ") está na categoria "
                     + desafiante.getCategoria() + ".");
-        }
+            this.setDesafiado(desafiado);
+            this.setDesafiante(desafiante);
+            this.lutaAprovada = false;
 
-        return false;
+            return false;
+        }
+    }
+
+    public void marcarLuta(Lutador desafiado, Lutador desafiante) {
+
+        if (isLutaAprovada(desafiado, desafiante)) {
+            apresentarLutadores();
+        }
+    }
+
+    public void apresentarLutadores() {
+
+        System.out.println("\nLadies and Gentlemen, please Welcome to Ultimate Emoji Championship!\n");
+        String[] corners = {"direito", "esquerdo"};
+
+        Lutador[] lutadores = {getDesafiado(), getDesafiante()};
+
+        for (Lutador lutador : lutadores) {
+
+            System.out.println("No corner " + corners[0] + "..."
+                    + "com " + lutador.getIdade() + " anos"
+                    + ", diretamente de " + lutador.getNacionalidade()
+                    + ", com altura de " + lutador.getAltura()
+                    + ", pesando " + lutador.getPeso() + " Kg. Com um total de " + lutador.getTotalLutas() + " lutas"
+                    + ", sendo " + lutador.getVitorias() + " vitórias, "
+                    + lutador.getDerrotas() + " derrotas e "
+                    + lutador.getEmpates() + " empates. Ele o peso " + lutador.getCategoria() + ", "
+                    + lutador.getNome() + "!");
+
+            corners[0] = corners[1];
+        }
+    }
+
+    public void lutar() {
+
+        if (lutaAprovada) {
+
+            Random gerarAleatorio = new Random();
+            int vencedor = gerarAleatorio.nextInt(3);
+
+            switch (vencedor) {
+
+                case 0: // empate
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    System.out.println(desafiado.getNome() + " e " + desafiante.getNome() + " empataram.");
+                    break;
+                case 1:
+                    this.desafiado.vencerLuta();
+                    this.desafiante.perderLuta();
+                    System.out.println(desafiado.getNome() + " venceu.");
+                    break;
+                case 2:
+                    this.desafiado.perderLuta();
+                    this.desafiante.vencerLuta();
+                    System.out.println(desafiante.getNome() + " venceu.");
+                    break;
+            }
+        }
     }
 }
--- src/main/java/br/com/simova/aula8b_video17/UECCombate.java
+++ src/main/java/br/com/simova/aula8b_video17/UECCombate.java
@@ -14,62 +14,28 @@ public class UECCombate {
         lutadores[4] = new Lutador("Ufocobol", "Brasil", 37, 170, 119.3f, 5, 4, 3);
         lutadores[5] = new Lutador("Nerdaart", "EUA", 30, 181, 105.7f, 12, 2, 4);
 
-        // luta não aprovada
-        Luta f1na = new Luta(lutadores[0], lutadores[2], 3);
-        apresentarLutadores(f1na);
+        Luta f1 = new Luta();
 
-        // luta aprovada após lutador desafiado terem peso e consequentemente categoria alterados para a mesma categoria do lutador desafiante
-        lutadores[0].setPeso(80);
-        apresentarLutadores(f1na);
+        f1.isLutaAprovada(lutadores[1], lutadores[2]);
 
-        // atribui o peso do lutador 0 para o mesmo peso que tinha anteriormente.
-        lutadores[0].setPeso(68.9f);
+        printarLinhaPontilhada();
+        lutadores[1].setPeso(80.9f);
+        f1.marcarLuta(lutadores[1], lutadores[2]);
+        lutadores[1].status();
+        lutadores[2].status();
 
-        // luta aprovada
-        Luta f1a = new Luta(lutadores[0], lutadores[1], 3);
-        Luta f2a = new Luta(lutadores[2], lutadores[3], 2);
-        Luta f3a = new Luta(lutadores[4], lutadores[5], 1);
+        printarLinhaPontilhada();
+        f1.lutar();
+        printarLinhaPontilhada();
 
-        apresentarLutadores(f1a);
-        apresentarLutadores(f2a);
-        apresentarLutadores(f3a);
+        f1.apresentarLutadores();
 
-        for (int i = 0; i < lutadores.length; i++) {
-
-            lutadores[i].status();
-            if (i < 2)
-                lutadores[i].vencerLuta();
-            if (i >= 2 && i <= 3)
-                lutadores[i].perderLuta();
-            if (i > 3)
-                lutadores[i].empatarLuta();
-            lutadores[i].status();
-        }
+        lutadores[1].status();
+        lutadores[2].status();
     }
 
-    public static void apresentarLutadores(Luta luta) {
+    public static void printarLinhaPontilhada() {
 
-        if (luta.isLutaAprovada()) {
-            System.out.println("\nLadies and Gentlemen, please Welcome to Ultimate Emoji Championship!\n");
-            String[] corners = {"direito", "esquerdo"};
-            Lutador[] lutadores = {luta.getDesafiado(), luta.getDesafiante()};
-
-            for (Lutador lutador : lutadores) {
-
-                System.out.println("No corner " + corners[0] + "..."
-                        + "com " + lutador.getIdade() + " anos"
-                        + ", diretamente de " + lutador.getNacionalidade()
-                        + ", com altura de " + lutador.getAltura()
-                        + ", pesando " + lutador.getPeso() + " Kg. Com um total de " + lutador.getTotalLutas() + " lutas"
-                        + ", sendo " + lutador.getVitorias() + " vitórias, "
-                        + lutador.getDerrotas() + " derrotas e "
-                        + lutador.getEmpates() + " empates. Ele o peso " + lutador.getCategoria() + ", "
-                        + lutador.getNome() + "!");
-
-                corners[0] = corners[1];
-            }
-
-            System.out.println("\n");
-        }
+        System.out.println("------------------------------------------------");
     }
 }
