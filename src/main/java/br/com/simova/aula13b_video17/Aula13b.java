5b6a065252f9067ffbf3902644d930956d0c4fb7
Robson Sousa <robson.sousa@simova.com.br>
2022-11-02 21:32:10 -0300

    'Aula 13b - vídeo 27': criação do método 'reagir' com diferentes assinaturas (tipos de dado, posição). Classes atualizadas: 'Aula13b' e 'Cachorro'.

--- src/main/java/br/com/simova/aula13b_video17/Aula13b.java
+++ src/main/java/br/com/simova/aula13b_video17/Aula13b.java
@@ -8,9 +8,40 @@ public class Aula13b {
         m1.emitirSom();
 
         Lobo l1 = new Lobo();
-        l1.emitirSom();;
+        l1.emitirSom();
 
         Cachorro c1 = new Cachorro();
         c1.emitirSom();
+
+        exibirLinhaPontilhada();
+
+        c1.reagir("Comida");
+        c1.reagir("Carinho");
+        c1.reagir("Quieto");
+
+        exibirLinhaPontilhada();
+
+        c1.reagir(11, 12);
+        c1.reagir(18, 1);
+        c1.reagir(14, 30);
+
+        exibirLinhaPontilhada();
+
+        c1.reagir(true);
+        c1.reagir(false);
+
+        exibirLinhaPontilhada();
+
+        c1.reagir(4, 9.5f);
+        c1.reagir(4, 10f);
+        c1.reagir(5, 9f);
+        c1.reagir(6, 10f);
+
+        exibirLinhaPontilhada();
+    }
+
+    public static void exibirLinhaPontilhada() {
+
+        System.out.println("----------------------------------------------------------------------");
     }
 }
--- src/main/java/br/com/simova/aula13b_video17/Cachorro.java
+++ src/main/java/br/com/simova/aula13b_video17/Cachorro.java
@@ -1,10 +1,55 @@
 package br.com.simova.aula13b_video17;
 
-public class Cachorro extends Lobo{
+public class Cachorro extends Lobo {
 
     @Override
     public void emitirSom() {
 
         System.out.println("Som de cachorro: Au! Au! Au!");
+    }
+
+    public void reagir(String frase) {
+
+        String[] frases = {"Comida", "Carinho"};
+
+        if (frase.equals(frases[0]) || frase.equals(frases[1]))
+            System.out.println("m1-1-> Abanar e latir!");
+        else
+            System.out.println("m1-2 -> Rosnar");
+    }
+
+    public void reagir(int hora, int minuto) {
+
+        if (hora < 12)
+            System.out.println("m2-1 -> Abanar o rabo");
+        else if (hora >= 18)
+            System.out.println("m2-2 -> Ignorar");
+        else
+            System.out.println("m2-3 -> Abanar e latir");
+    }
+
+    public void reagir(boolean dono) {
+
+        if (dono)
+            System.out.println("m3-1 -> Abanar");
+        else {
+            System.out.println("m3-2 -> Rosnar e latir");
+            emitirSom();
+        }
+
+
+    }
+
+    public void reagir(int idade, float peso) {
+
+        if (idade < 5) {
+            if (peso < 10)
+                System.out.println("m4-1 -> Abanar");
+            else
+                System.out.println("m4-2 -> Latir");
+        } else if (peso < 10)
+            System.out.println("m4-3 -> Rosnar");
+        else
+            System.out.println("m4-4 -> Ignorar");
     }
 }
